6adb553e3ff04528302131f7d53b1d10fc2deeee
SergioGF <mrsergiogf@gmail.com>
2018-04-22 17:17:15 +0200

    Nuevas funcionalidades.

---
+++ airbot.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-04-2018 a las 17:14:52
+-- Versión del servidor: 10.1.30-MariaDB
+-- Versión de PHP: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `airbot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recordatorios`
+--
+
+CREATE TABLE `recordatorios` (
+  `idreserva` int(11) NOT NULL,
+  `idusuario` int(11) NOT NULL,
+  `fecha` varchar(50) NOT NULL,
+  `hora` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id` int(11) NOT NULL,
+  `idvueloida` int(11) NOT NULL,
+  `idvueloretorno` int(11) DEFAULT NULL,
+  `idusuario` int(11) NOT NULL,
+  `fechareserva` varchar(50) NOT NULL,
+  `npersonas` int(11) NOT NULL,
+  `expirado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `reservas`
+--
+
+INSERT INTO `reservas` (`id`, `idvueloida`, `idvueloretorno`, `idusuario`, `fechareserva`, `npersonas`, `expirado`) VALUES
+(1, 1, NULL, 140760980, '21/04/2018 18:25', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`) VALUES
+(140760980);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vuelos`
+--
+
+CREATE TABLE `vuelos` (
+  `id` int(11) NOT NULL,
+  `origen` varchar(100) NOT NULL,
+  `destino` varchar(100) NOT NULL,
+  `fecha` varchar(50) NOT NULL,
+  `precio` double NOT NULL,
+  `plazas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `vuelos`
+--
+
+INSERT INTO `vuelos` (`id`, `origen`, `destino`, `fecha`, `precio`, `plazas`) VALUES
+(1, 'Madrid', 'Poznan', '22-04-2018 03:50', 45, 200);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `vuelos`
+--
+ALTER TABLE `vuelos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `vuelos`
+--
+ALTER TABLE `vuelos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- app.js
+++ app.js
@@ -1,6 +1,104 @@
 const TeleBot = require('telebot');
-const bot = new TeleBot('593194083:AAGUm1oWDfrgG5qhvuH5gvJk-Bn8toPNhm0');
+const bot = new TeleBot({
+    token: '593194083:AAGUm1oWDfrgG5qhvuH5gvJk-Bn8toPNhm0',
+    usePlugins: ['askUser', 'commandButton','namedButtons'],
+    pluginFolder: '../plugins/',
+    pluginConfig: {
+        // Plugin configs
+    }
+});
+var mysql = require('mysql');
+var messages = ['Bienvenido a Airbot, su asistente 24/7 para realizar reservas de vuelo. ¿Qué desea hacer?',
+				'Espero que nos volvamos a ver pronto. Ten un buen día.',
+				'Ayuda'
+				];
+				
+//Manejo de eventos
 
-bot.on('text', (msg) => msg.reply.text(msg.text));
+//Mensaje de bienvenida, también funciona con start.
+//En msg.from.id tenemos el ID de cada cliente que entra al chat, para poder guardarlo en la base de datos.
+bot.on(['/start', '/hola'], (msg) => (msg.reply.text(messages[0]) && insertarUsuarioBD(msg.from.id)));
+//Mensaje de despedida, también funciona con stop.
+bot.on(['/stop', '/adios'], (msg) => msg.reply.text(messages[1]));
+//Sección de ayuda de comandos.
+bot.on(['/help', '/ayuda'], (msg) => msg.reply.text(messages[2]));
+//Inicio del proceso de reserva
+bot.on(['/booking', '/reserva'], (msg) => "");
+//Consulta sobre un vuelo o reserva.
+bot.on(['/query', '/consulta'], (msg) => {
+
+    const replyMarkup = bot.inlineKeyboard([
+        [
+            bot.inlineButton('Vuelo', {callback: 'consultaVuelo("1")'})
+        ],
+        [
+            bot.inlineButton('Reserva', {callback: 'consultaReserva("1")'})
+        ]
+    ]);
+    return bot.sendMessage(msg.from.id, 'Sobre que desea realizar la consulta, ¿sobre un vuelo o una reserva?', {replyMarkup});
+});
+//Consultar/modificar recordatorio
+bot.on(['/reminder', '/recordatorio'], (msg) => "");
+//Pedir recomendaciones
+bot.on(['/recommendation', '/recomendacion'], (msg) => "");
+
+// Conexion con base de datos
+var connection = mysql.createConnection({ 
+   host: 'localhost',
+   user: 'root',
+   password: '',
+   database: 'airbot',
+});
+connection.connect(function(error){
+   if(error){
+      throw error;
+   }else{
+      console.log('Conexion correcta con con base de datos.');
+   }
+});
+
+//Consultas bd
+function insertarUsuarioBD(id){
+	
+   connection.query('SELECT COUNT(*) as usersCount FROM usuarios WHERE id=?', [id],function(err, rows, fields){
+	   if (err){
+		   throw err;
+	   }else{
+		   if(rows[0].usersCount < 1){
+			    connection.query('INSERT INTO usuarios(id) VALUES(?)', [id], function(error, result){
+				   if(error){
+					  throw error;
+				   }else{
+					  console.log('ID introducido correctamente.');
+				   }
+			})
+		   } else{
+			   console.log('El ID ya está dado de alta en la BD.');
+		   }
+	   }
+   });
+}
+
+function consultaVuelo(id){
+	console.log("aaa");
+	connection.query('SELECT * FROM vuelos WHERE id=?', [id],function(err, rows, fields){
+	   if (err){
+		   throw err;
+	   }else{
+		   return rows[0];
+	   }
+	});
+}
+
+function consultaReserva(id){
+	
+	connection.query('SELECT * FROM reservas WHERE id=?', [id],function(err, rows, fields){
+	   if (err){
+		   throw err;
+	   }else{
+		   return rows[0];
+	   }
+	});
+}
 
 bot.start();
