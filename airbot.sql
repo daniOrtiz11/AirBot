4b6f1e95877600a9cbf0d4e6a13f42e03e7aee5b
SergioGF <mrsergiogf@gmail.com>
2018-05-12 12:29:54 +0200

    BD

--- airbot.sql
+++ airbot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-04-2018 a las 11:27:10
+-- Tiempo de generación: 12-05-2018 a las 12:29:21
 -- Versión del servidor: 10.1.30-MariaDB
 -- Versión de PHP: 7.2.2
 
@@ -31,9 +31,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `recordatorios` (
   `idreserva` int(11) NOT NULL,
   `idusuario` int(11) NOT NULL,
-  `fecha` varchar(50) NOT NULL,
-  `hora` varchar(50) NOT NULL
+  `fechaRecordatorio` date NOT NULL,
+  `numeroDias` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `recordatorios`
+--
+
+INSERT INTO `recordatorios` (`idreserva`, `idusuario`, `fechaRecordatorio`, `numeroDias`) VALUES
+(1, 140760980, '2018-05-14', 2);
 
 -- --------------------------------------------------------
 
@@ -57,7 +64,20 @@ CREATE TABLE `reservas` (
 --
 
 INSERT INTO `reservas` (`id`, `idvueloida`, `idvueloretorno`, `idusuario`, `fechareserva`, `horareserva`, `npersonas`, `expirado`) VALUES
-(1, 1, NULL, 140760980, '21/04/2018 18:25', '11:14', 1, 0);
+(1, 1, NULL, 140760980, '21/04/2018 18:25', '11:14', 1, 0),
+(2, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '17:38', 2, 0),
+(3, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '20:39', 3, 0),
+(4, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '20:41', 3, 0),
+(5, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '20:45', 3, 0),
+(6, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '20:46', 3, 0),
+(7, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '20:47', 1, 0),
+(8, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '20:48', 1, 0),
+(9, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '20:50', 1, 0),
+(10, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '20:51', 1, 0),
+(11, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '20:52', 1, 0),
+(12, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '21:0', 1, 0),
+(13, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '21:3', 1, 0),
+(14, 1, NULL, 140760980, '2018-08-16 00:00:00.000', '12:44', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -97,7 +117,7 @@ CREATE TABLE `vuelos` (
 --
 
 INSERT INTO `vuelos` (`id`, `origen`, `destino`, `fecha`, `hora`, `precio`, `plazas`) VALUES
-(1, 'Madrid', 'Poznan', '2018-08-16', '15:30', 45, 200);
+(1, 'Madrid', 'Poznan', '2018-08-16', '15:30', 45, 187);
 
 --
 -- Índices para tablas volcadas
@@ -123,7 +143,7 @@ ALTER TABLE `vuelos`
 -- AUTO_INCREMENT de la tabla `reservas`
 --
 ALTER TABLE `reservas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `vuelos`
